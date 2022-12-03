776acdccaaf5e86a079d9218f587ea9392839fd7
Ciubix8513 <Ciubix8514@gmail.com>
2022-12-03 11:54:51 +0300

    Fixed CORS issue added self signed ssl

--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql;
+
+
 var ServerVersion = new MySqlServerVersion(new Version(10, 9, 4));
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                policy =>
+                {
+                    policy
+                    // .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .SetIsOriginAllowed(options => true)
+                    .AllowCredentials();
+                    //Not very safe but it's fine
+                });
+            });
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -11,24 +27,13 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAuthentication(
     CookieAuthenticationDefaults.AuthenticationScheme
 ).AddCookie();
-builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("CorsSpecs",
-                builder =>
-                {
-                    builder
-                    .AllowAnyHeader()
-                    .AllowAnyMethod()
-                    .SetIsOriginAllowed(options => true).
-                    AllowCredentials();
-                });
-            });
 builder.Services.AddDbContext<Api.Data.ApiAuthContext>(options =>
 {
     options.UseMySql(builder.Configuration.GetConnectionString("ApiAuthConnection"), ServerVersion);
 });
 var app = builder.Build();
-app.UseCors("CorsSpecs");
+app.UseCors();
+
 app.UseAuthentication();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
