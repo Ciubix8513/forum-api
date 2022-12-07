0a86417eb90fb35bce944ec64e9a62d70bc671b6
Luna <ciubix8514@gmail.com>
2022-12-08 00:46:35 +0300

    Removed sendgrid

--- Helpers/MailSender.cs
+++ Helpers/MailSender.cs
@@ -1,18 +1,14 @@
-using SendGrid;
-using SendGrid.Helpers.Mail;
-using System;
-using System.Threading.Tasks;
-
 namespace Api.Helpers;
 
 public static class MailSender
 {
     public static async Task SendEmail(string to,string subject, string body)
     {
-        var key = Environment.GetEnvironmentVariable("SG_KEY");
-        var client = new SendGridClient(key);
-        var from = new EmailAddress("noreply@ciubix.xyz");
-        var msg = MailHelper.CreateSingleEmail(from,  new EmailAddress(to), subject, body, body);
-        var response = await client.SendEmailAsync(msg);
+        // var key = Environment.GetEnvironmentVariable("SG_KEY");
+        // var client = new SendGridClient(key);
+        // var from = new EmailAddress("noreply@ciubix.xyz");
+        // var msg = MailHelper.CreateSingleEmail(from,  new EmailAddress(to), subject, body, body);
+        // var response = await client.SendEmailAsync(msg);
+        
     }
 }
